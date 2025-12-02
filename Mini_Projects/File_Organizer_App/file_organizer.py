37307d8976035255550a686e5e1875f4f9c323a3
Narasimha <satyansv5@gmail.com>
2025-12-02 16:09:59 +0530

    Day8

--- Mini_Projects/File_Organizer_App/file_organizer.py
+++ Mini_Projects/File_Organizer_App/file_organizer.py
@@ -12,3 +12,36 @@ Organize files into folders based on extension:
 
 Others → OTHERS/"
 """
+
+from pathlib import Path
+import shutil
+
+# Current working directory
+folder = Path(".")
+
+# Folder mapping based on extension
+mapping = {
+    ".txt": "TEXT",
+    ".jpg": "IMAGES",
+    ".png": "IMAGES",
+    ".py": "PYTHON",
+}
+
+# Loop through all files
+for file in folder.iterdir():
+
+    if file.is_file():
+        ext = file.suffix  # get file extension
+        
+        # Choose folder
+        if ext in mapping:
+            dest_folder = Path(mapping[ext])
+        else:
+            dest_folder = Path("OTHERS")
+        
+        # Create folder if not exist
+        dest_folder.mkdir(exist_ok=True)
+        
+        # Move file
+        shutil.move(str(file), dest_folder)
+        print(f"Moved {file} → {dest_folder}")
