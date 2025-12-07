fae5ecfe3d6e1f560a6f3e044c897a4c3a28e700
Narasimha <satyansv5@gmail.com>
2025-12-07 11:37:28 +0530

    .

--- Data Structures & Alogrithms/Twosum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums, target):
-        n = len(nums)
-        # Check every possible pair (i, j)
-        for i in range(n):
-            for j in range(i + 1, n):
-                # If the pair adds up to the target, return their indices
-                if nums[i] + nums[j] == target:
-                    return [i, j]
